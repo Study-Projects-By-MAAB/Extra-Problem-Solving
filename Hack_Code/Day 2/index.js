ebfb2a49bb623fea773779d34d510a4d71eee86d
maab-fw <maabfw@proton.me>
2024-03-18 15:44:03 +0600

    logic buliding/Hack_code day 2 task done

---
+++ Hack_Code/Day 2/index.js
@@ -0,0 +1,33 @@
+/* Next Day Problem
+
+Problem : 
+Find the palindrome of a given string/number
+
+Sample input:
+madam
+alia
+Sample output:
+true
+false
+*/
+const str1 = "madam"
+const str2 = "alia"
+
+function checkPalindrome(string) {
+    const splitedStr = string.split("")
+    let revStr = [];
+    for (let i = 0; i < splitedStr.length; i++) {
+        revStr.unshift(splitedStr[i])
+    }
+    const newStr = revStr.join("")
+
+    if (newStr === string) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+console.log(checkPalindrome(str1));
+console.log(checkPalindrome(str2));
